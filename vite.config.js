a28e6f0e65d840323153792324adf998595cd7fc
Willow-6855 <104696802+Willow-6855@users.noreply.github.com>
2025-08-26 19:55:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './',
+  root: "./src",
   build: {
-    outDir: 'dist',
+    outDir: "../dist",
     emptyOutDir: true,
-  },
-})
+    rollupOptions: {
+      input: resolve(__dirname, "src/index.html")
+    }
+  }
+});
